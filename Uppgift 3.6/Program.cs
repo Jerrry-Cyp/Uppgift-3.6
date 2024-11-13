e89e6530dd7b437acf205a567c86aa26b0dc75d8
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-13 12:48:12 +0100

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ditt namn:");
+            string N = Console.ReadLine();
+            string[] Namn = N.Split();
+            string Förnamn = Namn[0];
+            string Efternamn = Namn[1];
+            Console.WriteLine(Förnamn + " " + Efternamn);
+            if (Namn.Length > 2) Console.WriteLine("ERROR");
+        }
+    }
+}
